24377436ac762ed34e55c2a9ec3ea734ce33684e
Zebah Mamoon <zmamoon@usc.edu>
2025-04-14 05:01:20 +0000

    Completed HW5 - all tests passing

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,41 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper
+  (const AvailabilityMatrix& avail,
+  size_t dailyNeed,
+  size_t maxShifts,
+  DailySchedule& sched,
+  vector<size_t>& shiftCounts,
+  size_t day){
+
+    if(day == avail.size()){
+      return true; 
+    }
+
+    size_t numWorkers = avail[0].size();
+    vector<Worker_T>& today = sched[day]; 
+
+    for(size_t i =0; i < numWorkers; i++){
+      if(avail[day][i] && shiftCounts[i] < maxShifts && find(today.begin(), today.end(), i) == today.end()){
+        today.push_back(i);
+        shiftCounts[i]++;
+        if (today.size() == dailyNeed){
+          if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCounts, day + 1)){
+            return true; 
+          }
+        }
+        else{
+          if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCounts, day)){
+            return true; 
+          }
+        }
+        today.pop_back();
+        shiftCounts[i]--; 
+      }
+    }
+  return false; 
+}
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,7 +72,16 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t numDays = avail.size(); 
+    size_t numWorkers = avail[0].size(); 
+    sched.resize(numDays);
+    for(auto& day : sched){
+      day.reserve(dailyNeed); 
+    }
 
+    vector<size_t> shiftCounts(numWorkers, 0); 
+
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCounts, 0); 
 
 
 
--- wordle.cpp
+++ wordle.cpp
@@ -2,7 +2,7 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+//#include <algorithm> 
 #include <map>
 #include <set>
 #endif
@@ -11,18 +11,60 @@
 #include "dict-eng.h"
 using namespace std;
 
+size_t countDashes(const std::string& s){
+  int count = 0;
+  for(char c : s){
+    if(c == '-') count ++; 
+  }
+  return count; 
+}
 
 // Add prototypes of helper functions here
+void buildWords(std::string current, std::string floating, 
+  const std::set<std::string>& dict, 
+  std::set<std::string>& results, size_t index )
+  {
+    
+    if(index == current.size()){
+      if(floating.empty() && dict.find(current) != dict.end()){
+        results.insert(current); 
+      }
+      return; 
+    }
 
+    if (current[index] != '-'){
+      buildWords(current, floating, dict, results, index + 1);
+      return; 
+    }
 
-// Definition of primary wordle function
+    for(char c = 'a'; c <= 'z'; c++){
+      current[index] = c; 
+
+      std::string newFloating = floating;
+      size_t pos = newFloating.find(c);
+      if(pos != string::npos){
+        newFloating.erase(pos, 1);
+        buildWords(current, newFloating, dict, results, index +1); 
+      }
+      else{
+        if(newFloating.size() <= countDashes(current)){
+          buildWords(current, newFloating, dict, results, index + 1); 
+        }
+    }
+  }
+}
+  
+  
+  // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+  std::set<std::string> results;
+  buildWords(in, floating, dict, results, 0);
+  return results; 
 }
 
 // Define any helper functions here
